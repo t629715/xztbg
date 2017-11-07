294b0d0506322dc6aa68f4e971e1638f0bff8adf
lifangyin <17600089478@163.com>
2017-11-07 16:58:21 +0800

    部署到项目上

---
+++ src/main/webapp/static/css/theme-green/finacialTran.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="content">
+      <div id="content-header">
+        <div id="breadcrumb"> <a href="javascript:;" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 客户管理</a> <a href="javascript:;" class="current">客户分析</a> </div>
+        <h1></h1>
+      </div>
+      <div class="container-fluid">
+        理财交易
+
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
