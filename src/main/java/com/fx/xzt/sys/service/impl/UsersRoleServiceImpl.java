9feed9839e6e4c5e82655bd5cae0e96cad1061e0
tianliya <tianliya@163.com>
2017-12-18 09:32:53 +0800

    修复角色修改  默认选项为全选

--- src/main/java/com/fx/xzt/sys/service/impl/UsersRoleServiceImpl.java
+++ src/main/java/com/fx/xzt/sys/service/impl/UsersRoleServiceImpl.java
@@ -69,32 +69,39 @@ public class UsersRoleServiceImpl extends BaseService<UsersRole> implements User
 	 */
 	@Transactional
 	public int updateByIdSelective(UsersRole usersRole,List<Integer> pids) {
-		Date updateTime = new Date();
-		usersRole.setCreateTime(updateTime);
-		int msg = usersRoleMapper.updateByIdSelective(usersRole);
-		if(msg>0){
-			Integer rid = usersRole.getId();
-			if(pids!=null&&!pids.isEmpty()){
-				msg = usersRolePermissionService.deleteByRid(rid);
-				if(msg>0){
-					Set<Integer> set = new HashSet();
-					for (Integer pid : pids) {
-						set.add(usersPermissionMapper.getByPid(pid));
-					}
-					for (Integer i:set){
+		int msg = 0;
+		try{
+			Date updateTime = new Date();
+			usersRole.setCreateTime(updateTime);
+			 msg = usersRoleMapper.updateByIdSelective(usersRole);
+			if(msg>0){
+				Integer rid = usersRole.getId();
+				usersRolePermissionService.deleteByRid(rid);
+				if(pids!=null&&!pids.isEmpty()){
+
+					if(msg>0){
+						Set<Integer> set = new HashSet();
+						for (Integer pid : pids) {
+							set.add(usersPermissionMapper.getByPid(pid));
+						}
+					/*for (Integer i:set){
 						UsersRolePermission urp = new UsersRolePermission();
 						urp.setRid(rid);
 						urp.setPid(i);
 						msg = usersRolePermissionService.insertUsersRolePermission(urp);
-					}
-					for (Integer pid : pids) {
-						UsersRolePermission urp = new UsersRolePermission();
-						urp.setRid(rid);
-						urp.setPid(pid);
-						msg = usersRolePermissionService.insertUsersRolePermission(urp);
+					}*/
+						for (Integer pid : pids) {
+							UsersRolePermission urp = new UsersRolePermission();
+							urp.setRid(rid);
+							urp.setPid(pid);
+							msg = usersRolePermissionService.insertUsersRolePermission(urp);
+						}
 					}
 				}
 			}
+
+		}catch (Exception e){
+			e.printStackTrace();
 		}
 		return msg;
 	}
