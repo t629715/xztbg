9983e27285cecdd0cb1d5645851de81451e35f34
tianliya <tianliya@163.com>
2018-07-02 14:13:29 +0800

    修改配置文件为测试服务器

--- src/main/java/com/fx/xzt/sys/mapper/ActivityPrizeRuleMapper.java
+++ src/main/java/com/fx/xzt/sys/mapper/ActivityPrizeRuleMapper.java
@@ -3,6 +3,6 @@ package com.fx.xzt.sys.mapper;
 import com.fx.xzt.sys.entity.ActivityPrizeRule;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface ActivityPrizeRuleMapper extends Mapper<ActivityPrizeRule> {
+public interface ActivityPrizeRuleMapper extends BaseMapper<ActivityPrizeRule> {
     ActivityPrizeRule getByPrizeCode(String prizeCod);
 }
