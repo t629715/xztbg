a8b6ab18f300342f5fe7a30674e94c8a0e0fe8cd
PRC-20180616LBZ\Administrator <1711440445@qq.com>
2018-09-10 13:53:39 +0800

    改成 金权交割规则设定

---
+++ src/main/webapp/WEB-INF/template/dist/static/js/74.b11f35fa3d85e8e19f4f.js
@@ -0,0 +1,760 @@
+webpackJsonp([74],{
+
+/***/ 515:
+/***/ (function(module, exports, __webpack_require__) {
+
+
+/* styles */
+__webpack_require__(929)
+
+var Component = __webpack_require__(197)(
+  /* script */
+  __webpack_require__(602),
+  /* template */
+  __webpack_require__(848),
+  /* scopeId */
+  "data-v-fcd6c728",
+  /* cssModules */
+  null
+)
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ 602:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  //model 初始数据
+  data() {
+    return {
+      loading: true,
+      stateOptions: [{
+        value: "",
+        label: "全部"
+      }, {
+        value: 0,
+        label: '未使用'
+      }, {
+        value: 1,
+        label: '已使用'
+      }],
+      agentOptions: "",
+      brokerOptions: "",
+      sform: {
+        userName: '',
+        startTime: '',
+        endTime: '',
+        useStartTime: '',
+        useEndTime: '',
+        useStatus: '',
+        agentName: '',
+        brokerName: ''
+      },
+      url: this.api + "userVoucher/selectByUserVoucher",
+      agentUrl: this.api + "user/selectByAgentMessage",
+      brokeUrl: this.api + "user/selectByBrokerMessage",
+      brokerUrl1: this.api + "user/selectByBrokerMessage1",
+      exportUrl: this.api + "userVoucher/excelUserVoucher",
+      currentPage: 0,
+      pagesize: 10,
+      pageNum: 1,
+      totalNum: 0,
+      tableData: [],
+      dialogFormVisible: false,
+      dialogFormVisibleEdit: false,
+      formLabelWidth: '120px',
+      userId: ""
+    };
+  },
+  //页面渲染加载方法
+  created() {
+    this.loadData(10, 1);
+    let _this = this;
+    //初始化代理商下拉列表
+    axios.get(this.agentUrl).then(function (response) {
+      _this.agentOptions = response.data.data;
+    }).catch(function (error) {
+      console.log(error);
+    });
+    /*axios.get(this.brokerUrl1)
+        .then(function (response) {
+            _this.brokerOptions = response.data.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });*/
+  },
+  //定义方法
+  methods: {
+    //查询
+    onSubmit(sform) {
+
+      var params = new URLSearchParams();
+      let _this = this;
+      var date1 = '';
+      var date2 = '';
+      var date3 = '';
+      var date4 = '';
+
+      if (sform.startTime != "") {
+        date1 = this.dateFormat(sform.startTime);
+      }
+      if (sform.endTime != "") {
+        date2 = this.dateFormat(sform.endTime);
+      }
+      if (sform.useStartTime != "") {
+        date3 = this.dateFormat(sform.useStartTime);
+      }
+      if (sform.useEndTime != "") {
+        date4 = this.dateFormat(sform.useEndTime);
+      }
+
+      params.append('pageSize', this.pagesize);
+      params.append('pageNum', this.currentPage);
+      params.append('startTime', date1);
+      params.append('endTime', date2);
+      params.append('useStartTime', date3);
+      params.append('useEndTime', date4);
+      params.append('userName', sform.userName);
+      params.append('useStatus', sform.useStatus);
+      params.append('agentName', sform.agentName);
+      params.append('brokerName', sform.brokerName);
+
+      axios.post(this.url, params).then(function (response) {
+        if (response.data.code == 1001) {
+          _this.$message({
+            message: '查询成功',
+            type: 'success'
+          });
+          var list = response.data.data.list;
+          _this.currentPage = 1;
+          //_this.pagesize = response.data.data.pageSize;
+          _this.pageNum = response.data.data.pages;
+          _this.totalNum = response.data.data.total;
+          _this.handelData(list);
+          _this.tableData = list;
+        } else {
+          _this.$message({
+            message: '查询失败',
+            type: 'warning'
+          });
+        }
+      }).catch(function (error) {});
+    },
+    //刷新表格方法
+    loadData(pageSize, pageNum) {
+      var params = new URLSearchParams();
+      var date1 = '';
+      var date2 = '';
+      var date3 = '';
+      var date4 = '';
+
+      if (this.sform.startTime != "") {
+        date1 = this.dateFormat(this.sform.startTime);
+      }
+      if (this.sform.endTime != "") {
+        date2 = this.dateFormat(this.sform.endTime);
+      }
+      if (this.sform.useStartTime != "") {
+        date3 = this.dateFormat(this.sform.useStartTime);
+      }
+      if (this.sform.useEndTime != "") {
+        date4 = this.dateFormat(this.sform.useEndTime);
+      }
+
+      this.pagesize = pageSize;
+      this.currentPage = pageNum;
+      params.append('pageSize', this.pagesize);
+      params.append('pageNum', this.currentPage);
+      params.append('startTime', date1);
+      params.append('endTime', date2);
+      params.append('useStartTime', date3);
+      params.append('useEndTime', date4);
+      params.append('userName', this.sform.userName);
+      params.append('useStatus', this.sform.useStatus);
+      params.append('agentName', this.sform.agentName);
+      params.append('brokerName', this.sform.brokerName);
+      let _this = this;
+      axios.post(this.url, params).then(function (response) {
+        if (response.data.code == 1001) {
+          var list = response.data.data.list;
+          _this.handelData(list);
+          //_this.currentPage = response.data.data.pageNum == 0 ? 1 : response.data.data.pageNum;
+          //_this.pagesize = response.data.data.pageSize;
+          _this.pageNum = response.data.data.pages;
+          _this.totalNum = response.data.data.total;
+          _this.tableData = list;
+        }
+      }).catch(function (error) {});
+    },
+    //数据处理
+    handelData(list) {
+      if (list.length > 0) {
+        for (var i = 0; i < list.length; i++) {
+
+          //list[i].deductionValue = this.amountHandle1(list[i].deductionValue);
+          list[i].deductionPercent = this.isNotEmpty(list[i].deductionPercent) ? Number(list[i].deductionPercent) * 100 + '%' : 0;
+          if (list[i].useStatus == "0") {
+            list[i].useStatus = '未使用';
+          } else if (list[i].useStatus == "1") {
+            list[i].useStatus = '已使用';
+          }
+        }
+      }
+    },
+    //根据代理商取经纪人列表
+    getBrokerOptions() {
+      let _this = this;
+      _this.sform.brokerName = "";
+      var params = new URLSearchParams();
+      params.append('pid', _this.sform.agentName);
+      axios.post(_this.brokeUrl, params).then(function (response) {
+        _this.brokerOptions = response.data.data;
+      }).catch(function (error) {
+        console.log(error);
+      });
+    },
+    //清空
+    resetForm(sform) {
+      this.$refs.sform.resetFields();
+      this.loadData(10, 1);
+    },
+    //导出
+    onExport(sform) {
+      var params = new URLSearchParams();
+      let _this = this;
+      var date1 = '';
+      var date2 = '';
+      var date3 = '';
+      var date4 = '';
+
+      if (sform.startTime != "") {
+        date1 = this.dateFormat(sform.startTime);
+      }
+      if (sform.endTime != "") {
+        date2 = this.dateFormat(sform.endTime);
+      }
+      if (sform.useStartTime != "") {
+        date3 = this.dateFormat(sform.useStartTime);
+      }
+      if (sform.useEndTime != "") {
+        date4 = this.dateFormat(sform.useEndTime);
+      }
+
+      params.append('startTime', date1);
+      params.append('endTime', date2);
+      params.append('useStartTime', date3);
+      params.append('useEndTime', date4);
+      params.append('userName', sform.userName);
+      params.append('useStatus', sform.useStatus);
+      params.append('agentName', sform.agentName);
+      params.append('brokerName', sform.brokerName);
+
+      console.info(this.exportUrl + "?" + params);
+      window.location = this.exportUrl + "?" + params;
+    },
+    //当前页改变是执行
+    handleCurrentChange(val) {
+      /*this.currentPage = val;
+      this.onSubmit(this.sform);*/
+      this.loadData(this.pagesize, val);
+    },
+    //页数size 改变时执行
+    handleSizeChange(val) {
+      /*this.pageSize = val;
+      this.pageNum = 1;
+      this.onSubmit(this.sform);*/
+      this.loadData(val, 1);
+    },
+    //时间格式化
+    dateFormat(date) {
+      var dateobj = new Date(date);
+      var y = dateobj.getFullYear();
+      var m = dateobj.getMonth() + 1;
+      m = m < 10 ? '0' + m : m;
+      var d = dateobj.getDate();
+      d = d < 10 ? '0' + d : d;
+      var h = dateobj.getHours();
+      h = h < 10 ? '0' + h : h;
+      var minute = dateobj.getMinutes();
+      var second = dateobj.getSeconds();
+      minute = minute < 10 ? '0' + minute : minute;
+      second = second < 10 ? '0' + second : second;
+      return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 755:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(136)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ 848:
+/***/ (function(module, exports) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', {
+    attrs: {
+      "id": "content"
+    }
+  }, [_vm._m(0), _vm._v(" "), _c('div', {
+    staticClass: "container-fluid"
+  }, [_c('el-form', {
+    ref: "sform",
+    attrs: {
+      "inline": true,
+      "demo-form-inline": "",
+      "model": _vm.sform,
+      "label-width": "100px"
+    }
+  }, [_c('el-form-item', {
+    attrs: {
+      "label": "用户账号：",
+      "prop": "userName"
+    }
+  }, [_c('el-input', {
+    attrs: {
+      "size": "small"
+    },
+    model: {
+      value: (_vm.sform.userName),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "userName", $$v)
+      },
+      expression: "sform.userName"
+    }
+  })], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "label": "发放时间：",
+      "prop": "startTime"
+    }
+  }, [_c('el-col', {
+    attrs: {
+      "span": 10
+    }
+  }, [_c('el-date-picker', {
+    attrs: {
+      "size": "small",
+      "type": "datetime",
+      "placeholder": "选择日期时间",
+      "align": "right"
+    },
+    model: {
+      value: (_vm.sform.startTime),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "startTime", $$v)
+      },
+      expression: "sform.startTime"
+    }
+  })], 1)], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "prop": "endTime"
+    }
+  }, [_c('el-col', {
+    attrs: {
+      "span": 10
+    }
+  }, [_c('el-date-picker', {
+    attrs: {
+      "size": "small",
+      "type": "datetime",
+      "placeholder": "选择日期时间",
+      "align": "right"
+    },
+    model: {
+      value: (_vm.sform.endTime),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "endTime", $$v)
+      },
+      expression: "sform.endTime"
+    }
+  })], 1)], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "label": "使用时间：",
+      "prop": "useStartTime"
+    }
+  }, [_c('el-col', {
+    attrs: {
+      "span": 10
+    }
+  }, [_c('el-date-picker', {
+    attrs: {
+      "size": "small",
+      "type": "datetime",
+      "placeholder": "选择日期时间",
+      "align": "right"
+    },
+    model: {
+      value: (_vm.sform.useStartTime),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "useStartTime", $$v)
+      },
+      expression: "sform.useStartTime"
+    }
+  })], 1)], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "prop": "useEndTime"
+    }
+  }, [_c('el-col', {
+    attrs: {
+      "span": 10
+    }
+  }, [_c('el-date-picker', {
+    attrs: {
+      "size": "small",
+      "type": "datetime",
+      "placeholder": "选择日期时间",
+      "align": "right"
+    },
+    model: {
+      value: (_vm.sform.useEndTime),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "useEndTime", $$v)
+      },
+      expression: "sform.useEndTime"
+    }
+  })], 1)], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "label": "状态：",
+      "prop": "useStatus"
+    }
+  }, [_c('el-select', {
+    attrs: {
+      "size": "small",
+      "placeholder": "请选择"
+    },
+    model: {
+      value: (_vm.sform.useStatus),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "useStatus", $$v)
+      },
+      expression: "sform.useStatus"
+    }
+  }, _vm._l((_vm.stateOptions), function(item) {
+    return _c('el-option', {
+      key: item.id,
+      attrs: {
+        "label": item.label,
+        "value": item.value
+      }
+    })
+  }))], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "label": "代理商：",
+      "prop": "agentName"
+    }
+  }, [_c('el-select', {
+    attrs: {
+      "size": "small",
+      "placeholder": "请选择"
+    },
+    on: {
+      "change": function($event) {
+        _vm.getBrokerOptions()
+      }
+    },
+    model: {
+      value: (_vm.sform.agentName),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "agentName", $$v)
+      },
+      expression: "sform.agentName"
+    }
+  }, _vm._l((_vm.agentOptions), function(item) {
+    return _c('el-option', {
+      key: item.id,
+      attrs: {
+        "label": item.agentName,
+        "value": item.id
+      }
+    })
+  }))], 1), _vm._v(" "), _c('el-form-item', {
+    attrs: {
+      "label": "经纪人：",
+      "prop": "brokerName"
+    }
+  }, [_c('el-select', {
+    attrs: {
+      "size": "small",
+      "placeholder": "请选择"
+    },
+    model: {
+      value: (_vm.sform.brokerName),
+      callback: function($$v) {
+        _vm.$set(_vm.sform, "brokerName", $$v)
+      },
+      expression: "sform.brokerName"
+    }
+  }, _vm._l((_vm.brokerOptions), function(item) {
+    return _c('el-option', {
+      key: item.id,
+      attrs: {
+        "label": item.brokerName,
+        "value": item.id
+      }
+    })
+  }))], 1), _vm._v(" "), _c('el-form-item', [_c('el-button', {
+    attrs: {
+      "size": "small",
+      "type": "primary"
+    },
+    on: {
+      "click": function($event) {
+        _vm.onSubmit(_vm.sform)
+      }
+    }
+  }, [_vm._v("查询")])], 1), _vm._v(" "), _c('el-form-item', [_c('el-button', {
+    attrs: {
+      "size": "small",
+      "type": "danger"
+    },
+    on: {
+      "click": function($event) {
+        _vm.resetForm(_vm.sform)
+      }
+    }
+  }, [_vm._v("清除")])], 1), _vm._v(" "), _c('el-form-item', [_c('el-button', {
+    attrs: {
+      "size": "small",
+      "type": "info"
+    },
+    on: {
+      "click": function($event) {
+        _vm.onExport(_vm.sform)
+      }
+    }
+  }, [_vm._v("导出")])], 1)], 1), _vm._v(" "), _c('el-table', {
+    staticStyle: {
+      "width": "auto",
+      "display": "inline-block"
+    },
+    attrs: {
+      "data": _vm.tableData,
+      "border": "",
+      "stripe": ""
+    }
+  }, [_c('el-table-column', {
+    attrs: {
+      "type": "index",
+      "label": "序号",
+      "width": "100",
+      "fixed": "left"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "userName",
+      "label": "用户账号",
+      "width": "180",
+      "fixed": "left"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "agentName",
+      "label": "代理商",
+      "width": "180"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "brokerName",
+      "label": "经纪人",
+      "width": "180"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "description",
+      "label": "优惠券名称",
+      "width": "100"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "deductionPercent",
+      "label": "抵扣比例",
+      "width": "100"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "useTime",
+      "label": "使用时间",
+      "width": "180"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "createTime",
+      "label": "发放时间",
+      "width": "180"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "source",
+      "label": "来源",
+      "width": "180"
+    }
+  }), _vm._v(" "), _c('el-table-column', {
+    attrs: {
+      "prop": "useStatus",
+      "label": "状态",
+      "width": "100"
+    }
+  })], 1), _vm._v(" "), _c('el-pagination', {
+    attrs: {
+      "current-page": _vm.currentPage,
+      "page-sizes": [10, 20, 30, 40],
+      "page-size": _vm.pagesize,
+      "layout": "total, sizes, prev, pager, next, jumper",
+      "total": _vm.totalNum
+    },
+    on: {
+      "size-change": _vm.handleSizeChange,
+      "current-change": _vm.handleCurrentChange
+    }
+  })], 1)])
+},staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', {
+    attrs: {
+      "id": "content-header"
+    }
+  }, [_c('div', {
+    attrs: {
+      "id": "breadcrumb"
+    }
+  }, [_c('a', {
+    staticClass: "tip-bottom",
+    attrs: {
+      "href": "javascript:;",
+      "title": "Go to Home"
+    }
+  }, [_c('i', {
+    staticClass: "icon-home"
+  }), _vm._v(" 卡券记录")]), _vm._v(" "), _c('a', {
+    staticClass: "current",
+    attrs: {
+      "href": "javascript:;"
+    }
+  }, [_vm._v("优惠券")])]), _vm._v(" "), _c('h1')])
+}]}
+
+/***/ }),
+
+/***/ 929:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(755);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(198)("7f0577e0", content, true);
+
+/***/ })
+
+});
