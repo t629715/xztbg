a38a9dff06f69408ba658ae634656e5484be7862
zhufeng <zhufeng@163.com>
2018-10-31 15:55:15 +0800

    增加 官方支付宝（1）暂时没接入

--- src/main/java/com/fx/xzt/sys/util/ConstantUtil.java
+++ src/main/java/com/fx/xzt/sys/util/ConstantUtil.java
@@ -219,7 +219,8 @@ public class ConstantUtil {
         WX("微信","04"),
         ZFBSM("支付宝扫码","05"),
         GFZFB("官方支付宝","06"),
-        GFWX("官方微信","07");
+        GFWX("官方微信","07"),
+        GFZFB1("官方支付宝(1)","08");
         private String name;
         private String index;
         private rechargeChannel(String name,String index){
